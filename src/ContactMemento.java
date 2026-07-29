1e60ec5c3ec3f25fa318f6fe32282a5cabe29e0b
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-07-29 23:48:43 +0530

    [Gayatri:Added Contact Memento]

---
+++ src/ContactMemento.java
@@ -0,0 +1,14 @@
+// Stores the previous contact details
+
+public class ContactMemento {
+
+    private String name;
+
+    public ContactMemento(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
