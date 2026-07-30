6f6634e384e4209ca960be6e107785163223fb3a
Gayatri-M-Baligar <gayatribaligar2504@gmail.com>
2026-07-30 08:18:25 +0530

    [Gayatri: Added Phone Number Class]

---
+++ src/PhoneNumber.java
@@ -0,0 +1,17 @@
+public class PhoneNumber {
+
+    private String number;
+
+    public PhoneNumber(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return number;
+    }
+}
